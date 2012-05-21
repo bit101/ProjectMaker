f2626c47d42e56befe19f061f549a05c184c314a
Todd Anderson <bustardcelly@gmail.com>
2012-05-21 15:09:07 -0400

    initial commit of app stucture using RequireJS and Qunit.

---
+++ Templates/AppRequireJS/script/main.js
@@ -0,0 +1,35 @@
+( function( window, Modernizr ) {
+
+	"use strict";
+
+	Modernizr.load( [
+		/*
+		{
+			test: Modernizr[testSomething],
+			yep: filesToLoad
+		},
+		*/
+		{
+			load: ['lib/require.js'],
+			callback: function( file ) {
+				/* handler on async loaded file. */
+			},
+			complete: function() {
+
+				define.amd.jQuery = true;
+				require.config({
+					base: '.',
+					lib: './lib',
+					script: './script',
+					jquery: './lib/jquery'
+				});
+
+				require( ['jquery'], function( $ ) {
+					$('<p>AppRequireJS template for SublimeProjectMaker.</p>').appendTo( $("#placeholder") );
+				});
+			}
+		}
+	]);
+
+
+})( this, Modernizr );
