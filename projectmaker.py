ec866da473b51eae153424f25c846717b158f93f
bit101 <kp@bit-101.com>
2012-05-21 17:15:25 -0300

    Fixed bug that would show non-template items in template list. choose_template() checks if the file in question is a directory, but was not correctly joining the file name to the templates path. So it was always returning true.

--- projectmaker.py
+++ projectmaker.py
@@ -10,7 +10,7 @@ class ProjectMakerCommand(sublime_plugin.WindowCommand):
 	def choose_template(self):
 		files = self.get_templates()
 		for file_name in files:
-			if os.path.isdir(os.path.join(self.templates_path)):
+			if os.path.isdir(os.path.join(self.templates_path, file_name)):
 				self.template_names.append(file_name)
 		self.window.show_quick_panel(self.template_names, self.on_template_chosen)
 
